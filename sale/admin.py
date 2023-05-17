2c9aad8b326c644409148ec16d795cc1834f6c04
Pablo Sandoval <pablosandoval2191@gmail.com>
2023-05-17 17:52:10 -0500

    feat: models added in admin

--- sale/admin.py
+++ sale/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Product, Supplier
+
 
 # Register your models here.
+class SupplierAdmin(admin.ModelAdmin):
+    list_display = ("name", "last_name", "DNI")
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name", "price", "current_stock")
+
+
+admin.site.register(Supplier, SupplierAdmin)
+admin.site.register(Product, ProductAdmin) 
--- sale/models.py
+++ sale/models.py
@@ -6,6 +6,8 @@ class Supplier(models.Model):
     name = models.TextField(max_length=255)
     last_name = models.TextField(max_length=255)
     DNI = models.CharField(max_length=8)
+    def __str__(self) -> str:
+        return f"{self.last_name}, {self.name}"
 
 
 class Product(models.Model):
@@ -13,3 +15,5 @@ class Product(models.Model):
     price = models.FloatField()
     current_stock = models.IntegerField()
     supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE)
+    def __str__(self) -> str:
+        return self.name 
