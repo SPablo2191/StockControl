83497d6db8d2f7dc677c4ce1b099e8a1d1ad6659
Pablo Sandoval <pablosandoval2191@gmail.com>
2023-05-17 21:31:16 -0500

    feat: products list

--- sale/views.py
+++ sale/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.http import request,HttpResponse
+from .models import Product
+from django.template import loader
 # Create your views here.
+def get_products(request):
+    products = Product.objects.order_by('name')
+    template = loader.get_template("index.html")
+    context = {
+        "products": products,
+    }
+    return HttpResponse(template.render(context, request))
