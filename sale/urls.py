7f68e0a32f2ab9fd1f15ce053b46b450c40a68ff
Pablo Sandoval <pablosandoval2191@gmail.com>
2023-05-18 08:32:07 -0500

    feat: add product form added

--- sale/urls.py
+++ sale/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="home"),
     path("products", views.get_products, name="products"),
+    path("product", views.add_product, name="add_product"),
 ]
--- sale/views.py
+++ sale/views.py
@@ -1,15 +1,23 @@
 from django.shortcuts import render
-from django.http import request,HttpResponse
+from django.http import request, HttpResponse
 from .models import Product
 from django.template import loader
+
+
 # Create your views here.
 def index(request):
     template = loader.get_template("index.html")
     return HttpResponse(template.render())
+
+
 def get_products(request):
-    products = Product.objects.order_by('name')
+    products = Product.objects.order_by("name")
     template = loader.get_template("products.html")
     context = {
         "products": products,
     }
     return HttpResponse(template.render(context, request))
+
+def add_product(request):
+    template = loader.get_template("add_product.html")
+    return HttpResponse(template.render())
